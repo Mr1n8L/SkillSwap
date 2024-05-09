ba78f437a77cab44da253f15445ba857cca8a979
Mrinal <158752990+Mr1n8L@users.noreply.github.com>
2024-05-09 23:08:38 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-{/* Importing necessary modules */}
+{/* Import necessary modules */}
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
